76776021e2cd568de0e306f1533ad3985f50cf66
LeonardoEichs <leonardoeichs@gmail.com>
2016-09-06 02:37:55 -0300

    simple heuristic

--- Board.py
+++ Board.py
@@ -128,63 +128,83 @@ class Board():
         count = 0
 
         # Vencendo pela diagonal baixo direita
-        for i in range(1, 4):
+        for i in range(1, 5):
             if (m - i >= 0 and n - i >= 0 and self.board[m - i][n - i] == player):
-                count = count + 1
+                count = count + 10
+            elif (m - i >= 0 and n - i >= 0 and self.board[m - i][n - i] == 0):
+                count = count + 5
             else:
+                count = 0
                 break
         points["DiagonalCE"] = count
         count = 0
 
         # Vencendo pela diagonal cima direita
-        for i in range(1, 4):
+        for i in range(1, 5):
             if (m + i <= 14 and n - i >= 0 and self.board[m + i][n - i] == player):
-                count = count + 1
+                count = count + 10
+            elif (m + i <= 14 and n - i >= 0 and self.board[m + i][n - i] == 0):
+                count = count + 5
             else:
+                count = 0
                 break
         points["DiagonalBE"] = count
         count = 0
 
         # Vencendo pela diagonal baixo esquerda
-        for i in range(1, 4):
+        for i in range(1, 5):
             if (m - i >= 0 and n + i <= 14 and self.board[m - i][n + i] == player):
-                count = count + 1
+                count = count + 10
+            elif (m - i >= 0 and n + i <= 14 and self.board[m - i][n + i] == 0):
+                count = count + 5
             else:
+                count = 0
                 break
         points["DiagonalCD"] = count
         count = 0
 
         # Vencendo pela diagonal cima esquerda
-        for i in range(1, 4):
+        for i in range(1, 5):
             if (m + i <= 14 and n + i <= 14 and self.board[m + i][n + i] == player):
-                count = count + 1
+                count = count + 10
+            elif (m + i <= 14 and n + i <= 14 and self.board[m + i][n + i] == 0):
+                count = count + 5
             else:
+                count = 0
                 break
         points["DiagonalBD"] = count
         count = 0
 
         # Vencendo pela esquerda
-        for i in range(1, 4):
+        for i in range(1, 5):
             if (n + i <= 14 and self.board[m][n + i] == player):
-                count = count + 1
+                count = count + 10
+            elif (n + i <= 14 and self.board[m][n + i] == 0):
+                count = count + 5
             else:
+                count = 0
                 break
         points["Direita"] = count
         count = 0
 
         # Vencendo pela direita
-        for i in range(1, 4):
+        for i in range(1, 5):
             if (n - i >= 0 and self.board[m][n - i] == player):
-                count = count + 1
+                count = count + 10
+            elif (n - i >= 0 and self.board[m][n - i] == 0):
+                count = count + 5
             else:
+                count = 0
                 break
         points["Esquerda"] = count
         count = 0
 
         # Vencendo por Cima
-        for i in range(1, 4):
+        for i in range(1, 5):
             if (m + i <= 14 and self.board[m + i][n] == player):
-                count = count + 1
+                count = count + 10
+            elif (m + i <= 14 and self.board[m + i][n] == 0):
+                count = count + 5
             else:
                 break
         points["Baixo"] = count
@@ -193,7 +213,9 @@ class Board():
         # Vencendo por Baixo
         for i in range(1, 5):
             if (m - i >= 0 and self.board[m - i][n] == player):
-                count = count + 1
+                count = count + 10
+            elif (m - i >= 0 and self.board[m - i][n] == 0):
+                count = count + 5
             else:
                 break
         points["Cima"] = count
